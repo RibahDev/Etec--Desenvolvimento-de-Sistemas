bbb6cc9bef94f8883b01f64685c89cd76f9a6097
Ribeiro <joao.ribeiro17@outlook.com>
2023-10-11 22:19:48 -0300

    Iniciando projeto Quadrinhos

---
+++ 1° Modulo/Desenvolvimento Web/Quadrinhos/js/script.js
@@ -0,0 +1,38 @@
+fetch('quadrinhos.json')
+    .then(response => response.json())
+    .then(data => {
+        const comicList = document.querySelector('.comic-list');
+
+        data.quadrinhos.forEach(quadrinho => {
+            const listItem = document.createElement('li');
+            listItem.innerHTML =`<img src="${quadrinho.imagem}" alt="${quadrinho.titulo}">`;
+            listItem.addEventListener('click', () => showComicDetails(quadrinho));
+            comicList.appendChild(listItem);
+        });
+    });
+
+    function showComicDetails(quadrinho) {
+        const modal = document.querySelector('#comic-modal');
+        modal.innerHTML = `
+        <div class="modal-content">
+            <h2>${quadrinho.titulo}</h2> 
+            <p>${quadrinho.descricao}</p>
+            <igm src ="${quadrinho.imagem}" alt="${quadrinho.titulo}">
+            <button id="close-modal">Fechar</button>
+        </div>`;
+
+        modal.style.display = 'flex';
+
+        const closeButton = document.getElementById('close-modal');
+        closeButton.addEventListener('click', () => {
+            modal.style.display = 'none';
+        });
+    }
+    const modal = document.querySelector('#comic-modal');
+    window.addEventListener('click', (event) => {
+        if(event.target === modal) {
+            modal.style.display = 'none';
+        }
+    });
+
+    
