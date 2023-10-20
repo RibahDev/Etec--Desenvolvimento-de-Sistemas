c9d82a242f8bb75eb88b0f420a82033b82d266f0
RibahDev <joao.ribeiro17@outlook.com>
2023-10-19 21:30:46 -0300

    organizando

---
+++ 1° Modulo/Lógica e Algoritimos JS/Laços de repetição/while/ex-1/script.js
@@ -0,0 +1,9 @@
+// 1) Faça um algoritmo que imprima todos os números ímpares no intervalo entre 1 e 30 (incluindo ambos)
+let i = 1;
+debugger;
+while(i <= 30) {
+    if (i % 2 != 0) {
+        console.log(i);
+    }
+    i++;
+}
