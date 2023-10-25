6bd19af649fdef8d7cb2ddc3d9954c2f6922490b
RibahDev <joao.ribeiro17@outlook.com>
2023-10-24 23:51:09 -0300

    Finalizando ex 2, 24/10/23

---
+++ 1° Modulo/Lógica e Algoritimos JS/Laços de repetição/while/ex-10/script.js
@@ -0,0 +1,1 @@
+// 3) - Faça um programa que leia números inteiros maiores que zero. Quando for entrado o número zero, o programa deverá apresentar quantos números foram entrados e a média destes.
--- 1° Modulo/Lógica e Algoritimos JS/Laços de repetição/while/ex-9/script.js
+++ 1° Modulo/Lógica e Algoritimos JS/Laços de repetição/while/ex-9/script.js
@@ -1,29 +1,19 @@
 //  2) - 2)	Faça um programa que receba uma senha formada de quatro números inteiros, verifique se a senha está correta e, caso não esteja, solicite novamente a senha. Se a senha entrada for a correta, deverá ser apresentada a mensagem “Senha Correta”, caso contrário, “Senha Incorreta”.
 
 var senha = Number(prompt('Digite uma senha de até 4 Números:'));
-var senhaCorreta = senha;
 
-while(senha != senhaCorreta) {
-    if(senha.toString().length != 4){
-        alert("Sua senha deve conter 4 caracteres");
-    }
-
-    alert("Senha incorreta, Digite novamente!");
+while(senha.toString().length != 4){
+    alert("Sua senha deve conter 4 caracteres");
     senha = Number(prompt('Digite uma senha de até 4 Números:'));
 }
 
+var senhaCorreta = prompt("Confirme a senha:");
+
+while(senha != senhaCorreta) {
+
+    alert("Senha incorreta, Digite novamente!");
+
+    senhaCorreta = prompt("Confirme a senha:");
+}
+
 alert("Senha correta!");
-
-// ---------------------------------
-
-// var validando = 0;
-// var reverse = senha.toString();
-
-// else {
-//     prompt("Digite sua senha:");
-
-//     while(senha != senha) {
-//         alert("Senha incorreta, Digite novamente!");
-//         validando++;
-//     }
-// }
