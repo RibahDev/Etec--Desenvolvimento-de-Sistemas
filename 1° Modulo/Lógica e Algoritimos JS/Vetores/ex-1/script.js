f73dc055c6bf4a587aa4362d7c165fa326ca137f
Ribeiro <joao.ribeiro17@outlook.com>
2023-11-09 22:27:34 -0300

    Ex-1 Vetor

---
+++ 1° Modulo/Lógica e Algoritimos JS/Vetores/ex-1/script.js
@@ -0,0 +1,20 @@
+// 1) 1.	Crie um algoritmo para ler 5 números inteiros, armazene-os em um vetor, e mostre a quantidade de números pares e ímpares deste vetor.
+let x = [];
+let contadorI = null;
+let contadorP = null;
+
+
+for(let i = 0; i < 5; i++) {
+    let num = Number(prompt('Digite 5 nnúmeros:'));
+    x.push(num);
+}
+
+for(let i = 0; i < x.length; i++) {
+    if(x[i] % 2 == 0) {
+        contadorP++;
+    }else {
+        contadorI++;
+    } 
+}
+alert(contadorP);
+alert(contadorI);
