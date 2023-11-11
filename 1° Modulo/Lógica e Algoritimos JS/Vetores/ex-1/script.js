45fdde1d4d7c6add23e0c6e5541e100ddd0b3847
RibahDev <joao.ribeiro17@outlook.com>
2023-11-11 11:26:14 -0300

    exercicio 2

--- 1° Modulo/Lógica e Algoritimos JS/Vetores/ex-1/script.js
+++ 1° Modulo/Lógica e Algoritimos JS/Vetores/ex-1/script.js
@@ -5,7 +5,7 @@ let contadorP = null;
 
 
 for(let i = 0; i < 5; i++) {
-    let num = Number(prompt('Digite 5 nnúmeros:'));
+    let num = Number(prompt('Digite 5 números:'));
     x.push(num);
 }
 
--- 1° Modulo/Lógica e Algoritimos JS/Vetores/ex-2/script.js
+++ 1° Modulo/Lógica e Algoritimos JS/Vetores/ex-2/script.js
@@ -1,1 +1,10 @@
-//  2)
+//  2) Faça um algoritmo que leia 6 números e armazene em um vetor. Depois, some os 3 primeiros elementos deste vetor.
+let x = [];
+
+for(let i = 0; i < 6; i++) {
+    let num = Number(prompt('Digite 6 números:'));
+    x.push(num);
+}
+
+const valorX = x.slice(0, 3);
+console.log(valorX);
