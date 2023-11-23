324a05ffb671685b3d27577e5a8dcf47334d9e41
Ribeiro <joao.ribeiro17@outlook.com>
2023-11-23 20:11:26 -0300

    modal

---
+++ 1° Modulo/Desenvolvimento Web/Modal/js/script.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var clickableDivs = document.querySelectorAll('.abir-modal');
+    var modal = document.getElementById("modal");
+    var span = document.getElementsByClassName("fechar")[0];
+
+    clickableDivs.forEach(function(div) {
+        div.addEventListener('click', function() {
+            var imageUrl = this.getAttribute('data-image-url');
+            document.getElementById('modal-imagem').src = imageUrl;
+            modal.style.display = "flex";
+        });
+    });
+
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+
+    console.log('Modal displat style on load:', modal.style.display);
+});
