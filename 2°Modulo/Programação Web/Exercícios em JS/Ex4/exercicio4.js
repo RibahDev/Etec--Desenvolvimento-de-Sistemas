864141838925684e908281bdf0c5f179b7a45b37
Ribeiro <joao.ribeiro17@outlook.com>
2024-02-22 08:04:33 -0300

    Finalizando 4° exercicio Lógica em JS

--- 2°Modulo/Programação Web/Exercícios em JS/Ex4/exercicio4.js
+++ 2°Modulo/Programação Web/Exercícios em JS/Ex4/exercicio4.js
@@ -1,3 +1,11 @@
 /*4) Faça um algoritmo que receba 5 números inteiros, armazene-os em um vetor, em seguida, descubra e exiba o maior número. */
 
-let numeros = Number(prompt("Digite 5 números inteiros:"));
+let x = [];
+
+for(let i = 0; i < 5; i++) {
+    let num = Number(prompt(`Digite o ${i + 1}° número`));
+    x.push(num);
+}
+
+let maiorNumero = Math.max(...x);
+alert(`O maior Valor entre os números é: ${maiorNumero}`)
