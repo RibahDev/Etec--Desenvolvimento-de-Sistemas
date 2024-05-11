49355362746d23a5811a2e94f6040cadf9eade0a
Ribeiro <joao.ribeiro17@outlook.com>
2024-05-10 22:26:10 -0300

    Exercícips JS -if -do-while

--- 2°Modulo/C#/Aula1/obj/Debug/net8.0/Aula1.AssemblyInfo.cs
+++ 2°Modulo/C#/Aula1/obj/Debug/net8.0/Aula1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aula1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+688f0710ccd66625d72ea23647c1a2beabcc7bee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e2f02d09a95e8dca9e4b6b6f29ed8b446145230")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aula1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aula1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
